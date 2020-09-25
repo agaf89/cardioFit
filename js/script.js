44c70170b64e4fae34b8a6d59e1b4b5ebe7030fb
Stanislav <agafon_stas89@mail.ru>
2020-09-25 15:22:50 +0300

    'FINAL'

--- js/script.js
+++ js/script.js
@@ -43,41 +43,66 @@ $(document).ready(function(){
       $('.overlay, #order').fadeIn('fast');
     });
   });
-  $('#consultation-form').validate({
-    rules: {
-      name: "required",
-      phone: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      messages: {
-        name: "Пожалуйста, введите своё имя",
-        email: {
-          required: "Нам нужна почта для контакта с вами",
-          email: "Некорректный адрес почты"
+ 
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
         }
-      }
+    });
+  }
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+  $('form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn();
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+  //scroll&pageUp
+  $(window).scroll(function(){
+    if ($(this).scrollTop()> 1600){
+      $('.pageup').fadeIn();
+    } else{
+      $('.pageup').fadeOut();
     }
   });
-  $('#consultation form').validate({
-    rules: {
-      name: "required",
-      phone: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      messages: {
-        name: "Пожалуйста, введите своё имя",
-        email: {
-          required: "Нам нужна почта для контакта с вами",
-          email: "Некорректный адрес почты"
-        }
-      }
-    }
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
   });
-  $('#order form').validate();
+  new WOW().init();
 });
 
 
